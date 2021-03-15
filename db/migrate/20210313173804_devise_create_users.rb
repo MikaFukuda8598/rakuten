9d99f778c98e2d4f6aa75d8c61c3ccd453edaec4
EC2 Default User <ec2-user@ip-172-31-28-66.ap-northeast-1.compute.internal>
2021-03-15 13:12:49 +0000

    ユーザーテーブルの作成

--- db/migrate/20210313173804_devise_create_users.rb
+++ db/migrate/20210313173804_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name,              null: false
-　     t.string :postal_code,              null: false
-　     t.string :address,              null: false
-　     t.string :phone,              null: false
+      t.string :postal_code,       null: false
+      t.string :address,           null: false
+      t.string :phone,             null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -26,10 +26,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-        t.string   :confirmation_token
-        t.datetime :confirmed_at
-        t.datetime :confirmation_sent_at
-        t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -40,8 +40,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :email,                unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
